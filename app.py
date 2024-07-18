e7de4091b9d500e07b00696affdca188b1a33fa9
Mike <piotr@DESKTOP-435P9UT>
2024-07-18 19:11:08 +0200

    add templates

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, redirect, request, session, render_template
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from cs50 import SQL
+from helpers import apology, login_required
 
 app = Flask(__name__)
 
@@ -24,5 +25,91 @@ def after_request(response):
 
 
 @app.route("/")
+@login_required
 def index():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 400)
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return apology("invalid username and/or password", 400)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        # Ensure username was submitted
+        if not username:
+            return apology("must provide username", 400)
+
+        # Ensure password was submitted
+        elif not password:
+            return apology("must provide password", 400)
+        elif not confirmation:
+            return apology("must provide confirmation", 400)
+
+        elif not password == confirmation:
+            return apology("passwords doesn't match", 400)
+        # Check if username already exists
+        result = db.execute("SELECT 1 FROM users WHERE username = ?", (username,))
+        if list(result):  # Convert to list to see if any rows exist
+            return apology("username already exists", 400)
+
+        hash = generate_password_hash(password)
+        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, hash)
+
+        return redirect("/login")
+
+    else:
+        return render_template("register.html")
+    
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
